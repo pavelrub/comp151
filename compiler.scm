eefbc905119b9ca126dd2cae1e107318cf4bf0c4
Pavel Rubinson <pavelr@gmail.com>
2015-02-07 21:30:07 +0200

    first commit

---
+++ compiler.scm
@@ -0,0 +1,504 @@
+(load "pattern-matcher.scm")
+
+(print-graph #f) ; display circular structures
+(print-gensym #f) ; print gensym as g1234
+(case-sensitive #f) ; ditto
+(print-brackets #f) ; do not use brackets when pretty-printing
+
+(revert-interaction-semantics) ; allow builtins to be redefined
+
+;;; fix bug in optimizer
+(#%$sputprop 'append '*flags* 122)
+(#%$sputprop 'append! '*flags* 34)
+(#%$sputprop 'list* '*flags* 1250)
+(#%$sputprop 'cons* '*flags* 1250)
+
+;;; And just for good luck :-)
+(define with (lambda (s f) (apply f s)))
+
+(define simple-const?
+  (lambda (x)
+    (or (boolean? x) (char? x) (number? x) (string? x))))
+
+(define *reserved-words*
+  '(and begin cond define do else if lambda
+        let let* letrec or quasiquote unquote
+        unquote-splicing quote set!))
+
+(define not-reserved-word?
+  (lambda (x)
+    (if (memq x *reserved-words*)
+        #f
+        #t)))
+
+(define var?
+  (lambda (x)
+    (not (or (memq x *reserved-words*)
+             (pair? x)
+             (list? x)))))
+
+(define beginify
+  (lambda (a)
+    (cond ((null? a) (if #f #f))
+          ((null? (cdr a)) (car a))
+          (else `(begin ,@a)))))
+
+(define *void-object* (if #f #f))
+
+(define list-of-vars?
+  (lambda (x)
+    (and
+     (list? x)
+     (andmap var? x))))
+
+(define improper-list?
+  (lambda (x)
+    (and (pair? x) (not (list? x)))))
+
+(define last
+  (lambda (lst)
+    (cond ((not (pair? lst)) lst)
+          (else (last (cdr lst))))))
+
+(define all-but-last
+  (lambda (lst)
+    (cond ((pair? lst) (cons (car lst) (all-but-last (cdr lst))))
+          (else '()))))
+
+(define improper-list-of-vars?
+  (lambda (x)
+    (and
+     (improper-list? x)
+     (var? (last x))
+     (list-of-vars? (all-but-last x)))))
+
+(define has-duplicates?
+  (lambda (l)
+    (cond ((null? l) #f)
+          ((member (car l) (cdr l)) #t)
+          (else (has-duplicates? (cdr l))))))
+
+(define unique-cars?
+  (lambda (l)
+    (let ((cars (map car l)))
+      (not (has-duplicates? cars)))))
+
+(define with
+  (lambda (s f)
+    (apply f s)))
+
+(define Ym
+  (lambda fs
+    (let ((ms (map
+               (lambda (fi)
+                 (lambda ms
+                   (apply fi (map (lambda (mi)
+                                    (lambda args
+                                      (apply (apply mi ms) args))) ms))))
+               fs)))
+      (apply (car ms) ms))))
+
+(define expand-letrec
+  (lambda (letrec-expr)
+    (with letrec-expr
+          (lambda (_letrec ribs . exprs)
+            (let* ((fs (map car ribs))
+                   (lambda-exprs (map cdr ribs))
+                   (nu (gensym))
+                   (nu+fs `(,nu ,@fs))
+                   (body-f `(lambda ,nu+fs ,@exprs))
+                   (hofs
+                    (map (lambda (lambda-expr) `(lambda ,nu+fs ,@lambda-expr))
+                         lambda-exprs)))
+              `(Ym ,body-f ,@hofs))))))
+
+(define letrec?
+  (lambda (expr)
+    (and (pair? expr) (eq? (car expr) 'letrec))))
+
+(define expand-and
+  (lambda (and-expr)
+    `(if ,(car and-expr) ,(expand-and (cdr and-expr) '#f))))
+
+(define expand-qq
+  (lambda (e)
+    (cond ((unquote? e) (cadr e))
+          ((unquote-splicing? e)
+           (error 'expand-qq "unquote-splicing here makes no sense!"))
+          ((pair? e)
+           (let ((a (car e))
+                 (b (cdr e)))
+             (cond ((unquote-splicing? a) `(append ,(cadr a) ,(expand-qq b)))
+                   ((unquote-splicing? b) `(cons ,(expand-qq a) ,(cadr b)))
+                   (else `(cons ,(expand-qq a) ,(expand-qq b))))))
+          ((vector? e) `(list->vector ,(expand-qq (vector->list e))))
+          ((or (null? e) (symbol? e)) `',e)
+          (else e))))
+
+(define ^quote?
+  (lambda (tag)
+    (lambda (e)
+      (and (pair? e)
+           (eq? (car e) tag)
+           (pair? (cdr e))
+           (null? (cddr e))))))
+
+(define unquote? (^quote? 'unquote))
+(define unquote-splicing? (^quote? 'unquote-splicing))
+
+(define parse
+  (let ((run
+         (compose-patterns
+          (pattern-rule
+           (? 'c simple-const?)
+           (lambda (c) `(const ,c)))
+          (pattern-rule
+           `(quote ,(? 'c))
+           (lambda (c) `(const ,c)))
+          (pattern-rule
+           (? 'v var?)
+           (lambda (v) `(var ,v)))
+          (pattern-rule
+           `(if ,(? 'test) ,(? 'dit))
+           (lambda (test dit)
+             `(if3 ,(parse test) ,(parse dit) (const ,*void-object*))))
+          (pattern-rule
+           `(if ,(? 'test) ,(? 'dit) ,(? 'dif))
+           (lambda (test dit dif)
+             `(if3 ,(parse test) ,(parse dit) ,(parse dif))))
+          (pattern-rule
+           `(lambda ,(? 'args list-of-vars?) . ,(? 'body))
+           (lambda (args body)
+             `(lambda-simple ,args ,(parse (beginify body)))))
+          (pattern-rule
+           `(lambda ,(? 'args improper-list-of-vars?) . ,(? 'body))
+           (lambda (args body)
+             `(lambda-opt ,(all-but-last args) ,(last args) ,(parse (beginify body)))))
+          (pattern-rule
+           `(lambda ,(? 'args var?) . ,(? 'body))
+           (lambda (args body)
+             `(lambda-variadic ,args ,(parse (beginify body)))))
+          (pattern-rule
+           `(define ,(? 'var var?) ,(? 'expr))
+           (lambda (v expr)
+             `(define ,(parse v) ,(parse expr))))
+          (pattern-rule
+           `(define ,(? 'vars pair?) ,(? 'expr))
+           (lambda (vars expr)
+             `(define ,(parse (car vars)) ,(parse `(lambda ,(cdr vars) ,expr)))))
+          (pattern-rule
+           `(begin . ,(? 'lst list?))
+           (lambda (lst)
+             `(seq ,(map parse lst))))
+          (pattern-rule
+           `(,(? 'e1 not-reserved-word?) . ,(? 'args list?))
+           (lambda (e1 args)
+             `(applic ,(parse e1) ,(map parse args))))
+          (pattern-rule
+           `(or)
+           (lambda ()
+             (parse '#f)))
+          (pattern-rule
+           `(or ,(? 'expr))
+           (lambda (expr)
+             (parse expr)))
+          (pattern-rule
+           `(or . ,(? 'exprs))
+           (lambda (exprs)
+             `(or ,(map parse exprs))))
+          (pattern-rule
+           `(and)
+           (lambda ()
+             (parse '#t)))
+          (pattern-rule
+           `(and ,(? 'expr))
+           (lambda (expr)
+             (parse expr)))
+          (pattern-rule
+           `(and ,(? 'expr1) ,(? 'expr2))
+           (lambda (expr1 expr2)
+             (parse `(if ,expr1 ,expr2 #f))))
+          (pattern-rule
+           `(and ,(? 'expr) . ,(? 'rest))
+           (lambda (expr rest)
+             (parse `(if ,expr (and ,@rest) #f))))
+          (pattern-rule
+           `(let ,(? 'vars unique-cars?) . ,(? 'exprs))
+           (lambda (vars exprs)
+             (parse
+              `((lambda ,(map car vars) ,@exprs) ,@(map cadr vars)))))
+          (pattern-rule
+           `(let* () ,(? 'expr) . ,(? 'exprs list?))
+           (lambda (expr exprs)
+             (parse (beginify (cons expr exprs)))))
+          (pattern-rule
+           `(let* ((,(? 'var var?) ,(? 'val)) . ,(? 'rest)) . ,(? 'exprs))
+           (lambda (var val rest exprs)
+             (parse `(let ((,var ,val))
+                       (let* ,rest . ,exprs)))))
+          (pattern-rule
+           (? 'expr letrec?)
+           (lambda (expr)
+             (parse (expand-letrec `,expr))))
+          (pattern-rule
+           `(cond)
+           (lambda ()
+             `(const ,*void-object*)))
+          (pattern-rule
+           `(cond (else . ,(? 'exprs)))
+           (lambda (exprs)
+             (parse (beginify exprs))))
+          (pattern-rule
+           `(cond (,(? 't1) . ,(? 't1-rest)) . ,(? 'rest))
+           (lambda (t1 t1-rest rest)
+             (parse `(if ,t1 ,(beginify t1-rest) (cond ,@rest)))))
+          (pattern-rule
+           (list 'quasiquote (? 'sexpr))
+           (lambda (sexpr)
+             (parse (expand-qq sexpr))))
+          )))
+    (lambda (e)
+      (run e
+           (lambda ()
+             (error 'parse
+                    (format "I can't recognize this: ~s" e)))))))
+
+(define get-minor-acc
+  (lambda (var var-list minor)
+    (cond ((null? var-list) -1)
+          ((eq? (car var-list) var) minor)
+          (else (get-minor-acc var (cdr var-list) (+ minor 1))))))
+
+(define get-minor
+  (lambda (var var-list)
+    (get-minor-acc var var-list 0)))
+
+(define get-major-minor-acc
+  (lambda (var vars-mat major)
+    (if (null? vars-mat)
+        (list -1 -1)
+        (let ((minor (get-minor var (car vars-mat))))
+          (if (> minor -1)
+              (list major minor)
+              (get-major-minor-acc var (cdr vars-mat) (+ 1 major)))))))
+
+(define get-major-minor
+  (lambda (var vars-mat)
+    (get-major-minor-acc var vars-mat 0)))
+
+(define var->lex-var
+  (lambda (var pvars bvars)
+    (let ((pvar-index (get-minor var pvars)))
+      (if (> pvar-index -1)
+          `(pvar ,var ,pvar-index)
+          (let ((bvar-indexes (get-major-minor var bvars)))
+            (if (> (car bvar-indexes) -1)
+                `(bvar ,var ,@bvar-indexes)
+                `(fvar ,var)))))))
+
+(define get-tag
+  (lambda (l)
+    (if (list?  l)
+        (car l)
+        'error)))
+
+(define is-simple-lambda?
+  (lambda (exp)
+    (eq? (get-tag exp) 'lambda-simple)))
+
+(define get-lambda-simple-params
+  (lambda (lambda-exp)
+    (cadr lambda-exp)))
+
+(define get-lambda-simple-body
+  (lambda (lambda-exp)
+    (caddr lambda-exp)))
+
+(define is-opt-lambda?
+  (lambda (exp)
+    (eq? (get-tag exp) 'lambda-opt)))
+
+(define get-lambda-opt-params
+  (lambda (exp)
+    (append (cadr exp) (list (caddr exp)))))
+
+(define get-lambda-opt-body
+  (lambda (exp)
+    (cadddr exp)))
+
+(define is-var-lambda?
+  (lambda (exp)
+    (eq? (get-tag exp) 'lambda-variadic)))
+
+(define get-lambda-var-params
+  (lambda (exp)
+    (list (cadr exp))))
+
+(define get-lambda-var-body
+  (lambda (exp)
+    (caddr exp)))
+
+(define get-last
+  (lambda (l)
+    (car (reverse l))))
+
+(define get-all-but-last
+  (lambda (l)
+    (reverse (cdr (reverse l)))))
+
+(define pe->lex-pe*
+  (lambda (exp pvars bvars)
+    (cond ((null? exp) '())
+          ((not (list? exp)) exp)
+          ((is-simple-lambda? exp)
+           (let* ((params (get-lambda-simple-params exp))
+                  (body (get-lambda-simple-body exp))
+                  (bvars* (cons pvars bvars))
+                  (pvars* params)
+                  (all-but-last (get-all-but-last exp)))
+             (append all-but-last (list (pe->lex-pe* body pvars* bvars*)))))
+          ((is-opt-lambda? exp)
+           (let* ((params (get-lambda-opt-params exp))
+                  (body (get-lambda-opt-body exp))
+                  (bvars* (cons pvars bvars))
+                  (pvars* params)
+                  (all-but-last (get-all-but-last exp)))
+             (append all-but-last (list (pe->lex-pe* body pvars* bvars*)))))
+          ((is-var-lambda? exp)
+           (let* ((params (get-lambda-var-params exp))
+                  (body (get-lambda-var-body exp))
+                  (bvars* (cons pvars bvars))
+                  (pvars* params)
+                  (all-but-last (get-all-but-last exp)))
+             (append all-but-last (list (pe->lex-pe* body pvars* bvars*)))))
+          ((eq? (car exp) 'var) (var->lex-var (cadr exp) pvars bvars))
+          (else (cons (pe->lex-pe* (car exp) pvars bvars) (pe->lex-pe* (cdr exp) pvars bvars))))))
+
+
+(define pe->lex-pe
+  (lambda (exp)
+    (pe->lex-pe* exp '() '())))
+
+(define is-lambda?
+  (lambda (expr)
+    (let ((tag (get-tag expr)))
+      (or (eq? tag 'lambda-simple) (eq? tag 'lambda-opt) (eq? tag 'lambda-variadic)))))
+
+(define atp
+  (lambda (expr tp?)
+    (let ((tag (get-tag expr)))
+      (cond ((eq? tag 'const) expr)
+            ((eq? tag 'fvar) expr)
+            ((eq? tag 'pvar) expr)
+            ((eq? tag 'bvar) expr)
+            ((eq? tag 'var) expr)
+            ((eq? tag 'or)
+             (let ((all-but-last (get-all-but-last (cadr expr)))
+                   (last (get-last (cadr expr))))
+               `(or ,(append (map (lambda (x) (atp x #f)) all-but-last) (list (atp last tp?))))))
+            ((eq? tag 'if3)
+             (let ((test (cadr expr))
+                   (dit (caddr expr))
+                   (dif (cadddr expr)))
+               `(if3 ,(atp test #f) ,(atp dit tp?) ,(atp dif tp?))))
+            ((eq? tag 'define)
+             (let ((var (cadr expr))
+                   (peExpr (caddr expr)))
+               `(define ,var ,(atp peExpr #f))))
+            ((eq? tag 'seq)
+             (let ((all-but-last (get-all-but-last (cadr expr)))
+                   (last (get-last (cadr expr))))
+               `(seq ,(append (map (lambda (x) (atp x #f)) all-but-last) (list (atp last tp?))))))
+            ((is-lambda? expr)
+             (let ((body (get-last expr))
+                   (all-but-body (get-all-but-last expr)))
+               `(,@all-but-body ,(atp body #t))))
+            ((eq? tag 'applic)
+             (if (eq? tp? #f)
+                 `(applic ,(atp (cadr expr) #f) ,(map (lambda (x) (atp x #f)) (caddr expr)))
+                 `(tc-applic ,(atp (cadr expr) #f) ,(map (lambda (x) (atp x #f)) (caddr expr)))))))))
+
+(define annotate-tc
+  (lambda (expr)
+    (atp expr #f)))
+
+(define file->sexprs
+  (lambda (filename)
+    (let ((input (open-input-file filename)))
+      (letrec ((run
+                (lambda ()
+                  (let ((e (read input)))
+                    (if (eof-object? e)
+                        (begin (close-input-port input)
+                               '())
+                        (cons e (run)))))))
+        (run)))))
+
+(define nl (list->string (list #\newline)))
+(define prologue
+  (string-append
+   "#include <stdio.h>" nl
+   "#include <stdlib.h>" nl
+   "#include \"arch/cisc.h\"" nl
+   "" nl
+   "int main()" nl
+   "{" nl
+   "  START_MACHINE;" nl
+   "  JUMP(CONTINUE);" nl
+   "#include \"arch/char.lib\"" nl
+   "#include \"arch/io.lib\"" nl
+   "#include \"arch/math.lib\"" nl
+   "#include \"arch/string.lib\"" nl
+   "#include \"arch/system.lib\"" nl
+   "#include \"arch/scheme.lib\"" nl
+   nl
+   "CONTINUE:" nl
+   "/* definitions of some basic scheme objects */" nl
+   "/* this might be replaced later when symbols are properly implemented */" nl
+   nl
+   "  /* allocating 1000 memory cells */" nl
+   "  ADD(IND(0), IMM(1000));" nl 
+   nl
+   "  /* Void object definition */" nl
+   "  MOV(IND(1), IMM(T_VOID));" nl
+   "  #define SOB_VOID 1" nl
+   nl
+   "  /* Null (empty list) definition */" nl
+   "  MOV(IND(2), IMM(T_NIL));" nl 
+   "  #define SOB_NIL 2" nl 
+   nl
+   "  /* #t definition */" nl
+   "  MOV(IND(3), IMM(T_BOOL))" nl
+   "  MOV(IND(4), IMM(0))" nl
+   "  #define SOB_TRUE 3" nl 
+   nl
+   "  /* #f definition */" nl
+   "  MOV(IND(5), IMM(T_BOOL))" nl
+   "  MOV(IND(6), IMM(1))" nl
+   "  #define SOB_FALSE 5" nl
+   ))
+
+(define epilogue
+  (string-append
+   "  STOP_MACHINE;" nl
+   "  return 0;" nl
+   "}" nl))
+
+
+;(define code-gen
+;  (lambda (pe env-size param-size)
+;    (cond
+;     ((pe-const? pe) ______________)
+;     ((pe-var? pe) ______________)
+;     .
+;     .
+;     .
+;     (else (error ...)))))
+(define write-to-file-test
+  (lambda (filename string)
+    (let ((p (open-output-file filename '(replace))))
+      (begin
+        (display string p)
+        (close-port p)))))
+;(define compile-scheme-file
+ ;;; (lambda (source target)
