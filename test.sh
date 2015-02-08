b5c372ce41179d74ebb1c4c6c886589d61a1754b
Pavel Rubinson <pavelr@gmail.com>
2015-02-08 23:46:08 +0200

    Make clean before running tests

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 # The expected tests are .scm file: just files with scheme expressions. 
 # The script compiles the files to pseudo-assembly, then to an executable using a makefile, then compares the outputs of the executables
 # with the outputs creates by Chez. Diff output is given.
-
+(cd $1 && make clean)
 for f in ${1}/*.scm;
 do
   echo "(compile-scheme-file \"${f}\" \"${f%.scm}.c\")" | petite compiler.scm -q
